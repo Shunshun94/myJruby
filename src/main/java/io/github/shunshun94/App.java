5a765cbbaf79364e4f7a7c7ad436b885ca240a82
Shunshun94 <shunshun9460@gmail.com>
2020-04-11 19:07:01 +0900

    BCDice roll calling

--- src/main/java/io/github/shunshun94/App.java
+++ src/main/java/io/github/shunshun94/App.java
@@ -16,32 +16,29 @@ public class App
     	ScriptEngineManager manager = new ScriptEngineManager();
     	ScriptEngine engine = manager.getEngineByName("jruby");
     	try {
-			engine.put("msg", "Hello");
-			engine.eval("puts msg");
-			engine.eval("msg = 'Bye'");
-			Object result1 = engine.get("msg");
-			System.out.println(result1.getClass().getName() + ": " + result1);
-
-			engine.put("count", 0);
-			engine.eval("count += 3");
-			engine.eval("puts count");
-			engine.eval("count += 4");
-			Object result2 = engine.get("count");
-			System.out.println(result2.getClass().getName() + ": " + result2);
-			System.out.println((Long)result2 + 10);
-
 			// 実行ディレクトリを MyJRuby/src/main/resources/BCDice/src にしている
-			// ソースコードは https://github.com/ysakasin/bcdice-api/blob/master/lib/bcdice_wrap.rb からコピペ
+			// ソースコードは https://github.com/ysakasin/bcdice-api/blob/master/lib/bcdice_wrap.rb からコピペしたのをいじいじ。
 			engine.eval("$:.unshift File.dirname(__FILE__)");
 			engine.eval("require \"./bcdiceCore\"\nrequire \"diceBot/DiceBot\"\nrequire \"diceBot/DiceBotLoader\"");
 			engine.eval("DICEBOTS = ([DiceBot.new] + DiceBotLoader.collectDiceBots).\r\n" + 
 					"    map { |diceBot| [diceBot.id, diceBot] }.\r\n" + 
 					"    to_h.\r\n" + 
-					"    freeze");
-			engine.eval("SYSTEMS = DICEBOTS.keys.\r\n" + 
+					"    freeze\r\n\r\n" +
+			        "    SYSTEMS = DICEBOTS.keys.\r\n" + 
 					"    sort.\r\n" + 
-					"    freeze");
-			engine.eval("puts SYSTEMS");
+					"    freeze\r\n\r\n" +
+					"    NAMES = DICEBOTS.\r\n" + 
+					"    map { |gameType, diceBot| {system: diceBot.id, name: diceBot.name} }.\r\n" + 
+					"    freeze\r\n" + 
+					"");
+			engine.eval("dicebot = DICEBOTS['SwordWorld2.0']");
+			engine.eval("bcdice = BCDiceMaker.new.newBcDice");
+			engine.eval("dicebot.bcdice = bcdice");
+			engine.eval("puts dicebot.dice_command('aaaaa', 'SwordWorld2.0')");
+			engine.eval("puts dicebot.dice_command('FT', 'SwordWorld2.0')");
+			engine.eval("puts dicebot.dice_command('2d6', 'SwordWorld2.0')");
+			engine.eval("puts dicebot.dice_command('K20', 'SwordWorld2.0')");
+			engine.eval("puts dicebot.dice_command('k20', 'SwordWorld2.0')");
 		} catch (ScriptException e) {
 			e.printStackTrace();
 		}
